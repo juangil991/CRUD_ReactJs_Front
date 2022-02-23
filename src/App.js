b20e760094babbce64fde5d2a7121a50bb1aafbc
juangil991 <jcgo125@gmail.com>
2022-02-23 12:06:01 -0500

    se añade el provider y el use effect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,74 @@
+import React, { createContext, useContext, useReducer, useEffect } from "react";
+
+const HOST_API = "http://localhost:8080/api";
+const initialState = {
+  list: [],
+};
+const Store = createContext(initialState);
+
+const List = () => {
+  const { dispatch, state } = useContext(Store);
+
+  useEffect(()=>{
+    fetch(HOST_API+"/todos")
+    .then(response => response.json())
+    .then((list)=>{
+      dispatch({type: "update-list",list})
+    })
+  })
+
+
+  return (
+    <div>
+      <table>
+        <thead>  
+          <tr>
+            <td>ID</td>
+            <td>Nombre</td>
+            <td>¿Esta completado?</td>
+          </tr>
+        </thead>
+        <body>
+          {state.list.map((todo) => {
+            return (
+              <tr key={todo.id}>
+                <td>{todo.id}</td>
+                <td>{todo.name}</td>
+                <td>{todo.isCompleted}</td>
+              </tr>
+            );
+          })}
+        </body>
+      </table>
+    </div>
+  );
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "update-list":
+      return { ...state, list: action.list };
+    case "add-item":
+      const newList = state.list;
+      newList.push(action.item);
+      return { ...state, list: newList };
+    default:
+      return state;
+  }
+}
+const StoreProvider = ({ children }) => {
+ const[state, dispatch]= useReducer(reducer,initialState);
+
+ return <Store.Provider value={{state, dispatch}}>
+  {children}
+ </Store.Provider>
+ };
 
 
 function App() {
-  return (
-    <div className="App">
-        hola mundo
-    </div>
-  );
+  return <StoreProvider>
+
+  </StoreProvider>
 }
 
 export default App;
